5d23f92c1a2cf5faf080fc4c71a1511a77ee93ec
asemru <95555506+asemru@users.noreply.github.com>
2024-09-08 18:33:01 +1200

    Update pupkin.py

--- pupkin.py
+++ pupkin.py
@@ -1,15 +1,46 @@
-def all_variants(text):
-    for i in text:
-        yield i
-    for g in range(0, len(text) - 1):
-        yield text[g] + text[g+1]
-    yield text
+import threading
+import random
+import time
+class Bank:
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.balance = 0
+
+    def deposit(self):
+        for __ in range(1, 101):
+            i = random.randint(50, 500)
+            if self.lock.locked() and self.balance >= 500:
+                self.lock.release()
+            self.balance += i
+            print(f'Пополнение: {i}, Баланс: {self.balance}')
+            time.sleep(0.001)
+
+
+    def take(self):
+        for _ in range(1, 101):
+            i1 = random.randint(50, 500)
+            print(f'Запрос на -> {i1}')
+            if i1 <= self.balance:
+                self.balance -= i1
+                print(f'Снятие: {i1}, Ваш текуший баланс: {self.balance}')
+            else:
+                print('Запрос отклонён, недостаточно средств')
+                self.lock.acquire()
 
 
 
-a = all_variants("abc")
-for i in a:
-    print(i)
+bk = Bank()
+
+# Т.к. методы принимают self, в потоки нужно передать сам объект класса Bank
+th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+th2 = threading.Thread(target=Bank.take, args=(bk,))
+
+th1.start()
+th2.start()
+th1.join()
+th2.join()
+
+print(f'Итоговый баланс: {bk.balance}')
 
 
 
