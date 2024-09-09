e3bda74d18f99a10efadfca88ce5507aa928875e
asemru <95555506+asemru@users.noreply.github.com>
2024-09-09 21:36:46 +1200

    Update pipi.py

--- pipi.py
+++ pipi.py
@@ -1,31 +1,81 @@
-from threading import Thread
-import time
-class Knight(Thread):
-    def __init__(self, name, power):
+import threading
+from queue import Queue
+from time import sleep
+from random import randint
+
+
+class Cafe:
+    ry = []
+    def __init__(self, *tables):
+        self.queue = Queue()
+        self.table = tables
+
+    def guest_arrival(self, *guests):
+        for guest in guests:
+            for table in self.table:
+                if table.guest == None:
+                    table.guest = guest.name
+                    guest.start()
+                    print(f'{table.guest} сел(-а) за стол номер {table.number}')
+                    self.ry.append(guest)
+                    break
+            if not guest.is_alive():
+                self.queue.put(guest)
+                print(f'{guest.name} в очереди')
+
+    def discuss_guests(self):
+        flag = True
+        while not self.queue.empty() or flag:
+            for table in self.table:
+                guest_name = table.guest
+                for guest in self.ry:
+                    if guest_name == guest.name:
+                        if not guest.is_alive():
+                            print(f'{guest.name} покушал(-а) и ушёл(ушла)')
+                            print(f'Стол номер {table.number} свободен')
+                            table.guest = None
+                            guest.join()
+                            if not self.queue.empty():
+                                guest = self.queue.get()
+                                print(f'{guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}')
+                                table.guest = guest.name
+                                guest.start()
+                        break
+            flag = False
+            for table in self.table:
+                if not table.guest is None:
+                    flag = True
+                    break
+
+
+
+class Guest(threading.Thread):
+    def __init__(self, name):
         super().__init__()
         self.name = name
-        self.power = power
-
 
     def run(self):
-        HP = 100
-        Day = 0
-        print(f'{self.name} на нас напали!')
-        while HP > 0:
-            time.sleep(1)
-            HP -= self.power
-            Day += 1
-            print(f'{self.name} сражается {Day} день(дня)..., осталось {HP}')
-        print(f'{self.name} одержал победу спустя {Day} дней(дня)')
+        sleep(randint(3, 10))
+
+class Table:
+    def __init__(self, number, guest=None):
+        self.number = number
+        self.guest = guest
 
 
-first_knight = Knight('Sir Lancelot', 10)
-second_knight = Knight("Sir Galahad", 20)
 
-
-first_knight.start()
-second_knight.start()
-
-first_knight.join()
-second_knight.join()
-
+# Создание столов
+tables = [Table(number) for number in range(1, 6)]
+# Имена гостей
+guests_names = [
+'Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman',
+'Vitoria', 'Nikita', 'Galina', 'Pavel', 'Ilya', 'Alexandra'
+]
+# Создание гостей
+guests = [Guest(name) for name in guests_names]
+# Заполнение кафе столами
+cafe = Cafe(*tables)
+# Приём гостей
+cafe.guest_arrival(*guests)
+# Обслуживание гостей
+cafe.discuss_guests()
