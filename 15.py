49967a0bb7578e024d166a81eada092271b1e279
asemru <95555506+asemru@users.noreply.github.com>
2024-09-15 14:16:04 +1200

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,29 @@
+import asyncio
+import time
+async def start_strongman(name, power):
+    print(f'Силач {name} начал соревнования')
+    for _ in range(1, 6):
+        await asyncio.sleep(1 / int(power))
+        print(f'Силач {name} поднял {_} шар')
+    print(f'Силач {name} закончил соревнование')
+
+
+async def start_tournament():
+    t1 = asyncio.create_task(start_strongman('Pasha', 3))
+    t2 = asyncio.create_task(start_strongman('Denis', 4))
+    t3 = asyncio.create_task(start_strongman('Apollon', 5))
+    await t1
+    await t2
+    await t3
+    print('финиш')
+
+
+start = time.time()
+asyncio.run(start_tournament())
+finish = time.time()
+print(round(finish - start, 2))
+
+
+
+
+
