6503291b013279b421e517dae3ddec651655df64
asemru <95555506+asemru@users.noreply.github.com>
2024-09-17 23:14:46 +1200

    Add files via upload

--- 13.py
+++ 13.py
@@ -2,17 +2,26 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 import asyncio
 
-api = '' #<-- Вставить номер своего телеграмм бота
+api = '7346715807:AAGlBVpL6ZvgQ8ZZTvaGlnCMHMAxK08ZEw4' #<-- Вставить номер своего телеграмм бота
 bot = Bot(token=api)
 db = Dispatcher(bot, storage=MemoryStorage())
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button1 = KeyboardButton('Рассчитать')
+button2 = KeyboardButton('Информация')
+kb.add(button1, button2)
 
 class UserState(StatesGroup):
     age = State()
     growth = State()
     weight = State()
 
+
+@db.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Нажмите на кнопку - Рассчитать', reply_markup=kb)
 
 @db.message_handler(state=UserState.weight)
 async def send_calories(message, state):
@@ -35,12 +44,14 @@ async def set_growth(message, state):
     await message.answer('Введите свой рост')
     await UserState.growth.set()
 
-
-
-@db.message_handler(text=['Calories'])
+@db.message_handler(text='Рассчитать')
 async def set_age(message):
     await message.answer('Введите свой возраст')
     await UserState.age.set()
+
+@db.message_handler()
+async def tup(message):
+    await message.answer('Введите команду /start, чтобы начать общение')
 
 
 if __name__ == '__main__':
