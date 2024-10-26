9a7f6fa78670e01e5ed5cbb3614e621a97da6281
asemru <95555506+asemru@users.noreply.github.com>
2024-10-26 19:05:58 +1200

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,11 @@
-import sys
-from pprint import *
-from inspect import getmodule
-import math
+from fastapi import FastAPI
+from homework2 import task, user
 
-def introspection_info(obj):
-    a = {'type': type(obj).__name__}
-    try:
-        a['модуль'] = obj.__module__
-    except:
-        a['модуль'] = '__main__'
-    a['методы'] = dir(obj)[0], dir(obj)[1] #Выводит только два первых так как если вывести всё то в консоль выведится очень длиная строка
-    try:
-        a['атрибуты'] = vars(obj)
-    except:
-        a['атрибуты'] = '[...]'
+apo = FastAPI()
 
-    return a
-
-
-
-number_info = introspection_info(42)
-print(number_info)
-
-
-
+@apo.get('/')
+async def welcome():
+    return {'message': 'welcome, this is my project'}
 
 
 
