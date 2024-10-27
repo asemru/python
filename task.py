dfaa82a9a207f5999c7e52a553fc160ebb0205c9
asemru <95555506+asemru@users.noreply.github.com>
2024-10-27 16:34:54 +1200

    Add files via upload

--- task.py
+++ task.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter
 from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Float
 from sqlalchemy.orm import relationship
 from sqlalchemy.schema import CreateTable
-from homework.backend import db
+from homework.backend.db import *
+
 api = APIRouter(prefix='/task', tags=['task'])
 
-class Task(db.Base):
+class Task(Base):
     __tablename__ = 'task'
     id = Column(Integer, index=True, primary_key=True)
     title = Column(String)
@@ -16,19 +17,7 @@ class Task(db.Base):
     slug = Column(String, index=True, unique=True)
     user = relationship('User', back_populates='tasks')
 
-class User(db.Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String)
-    firstname = Column(String)
-    lastname = Column(String)
-    age = Column(Integer)
-    slug = Column(String, index=True, unique=True)
-    tasks = relationship('Task', back_populates='user')
 
-
-print(CreateTable(User.__table__))
-print(CreateTable(User.__table__))
 
 @api.get('/')
 async def all_task():
--- user.py
+++ user.py
@@ -1,6 +1,24 @@
 from fastapi import APIRouter
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+from sqlalchemy.schema import CreateTable
+from homework.backend import db
+
 
 app = APIRouter(prefix='/user', tags=['user'])
+
+class User(db.Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, index=True, unique=True)
+    tasks = relationship('Task', back_populates='user')
+
+
+
 
 @app.get('/')
 async def all_task():
